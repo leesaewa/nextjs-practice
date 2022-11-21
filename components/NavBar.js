830b8ec06ff874023632d4b0e29bc1251630d636
leesaewa <saewa0424@gmail.com>
2022-11-22 00:05:59 +0900

    test commit

---
+++ components/NavBar.js
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export default function NavBar() {
+  const router = useRouter();
+  console.log(router);
+
+  return (
+    <nav>
+      <Link href="/" legacyBehavior>
+        <a>Home</a>
+      </Link>
+      <Link href="/about" legacyBehavior>
+        <a>About</a>
+      </Link>
+    </nav>
+  );
+}
